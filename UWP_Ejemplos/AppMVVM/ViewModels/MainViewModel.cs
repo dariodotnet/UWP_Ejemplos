75746adabcaeed476147308942a30b6d3f418767
Dario Alonso <dario@DESKTOP-22IF92Q>
2017-01-17 10:00:10 +0100

    Agregados los comandos

--- UWP_Ejemplos/AppMVVM/ViewModels/MainViewModel.cs
+++ UWP_Ejemplos/AppMVVM/ViewModels/MainViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using AppMVVM.Base;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 namespace AppMVVM.ViewModels
@@ -16,9 +19,33 @@ namespace AppMVVM.ViewModels
                 RaisePropertyChanged();
             }
         }
+
+        private SolidColorBrush _appBackground;
+        public SolidColorBrush AppBackground
+        {
+            get { return _appBackground; }
+            set
+            {
+                _appBackground = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private ICommand changeColorCommand;
+        public ICommand ChangeColorCommand
+        {
+            get { return changeColorCommand = changeColorCommand ?? new DelegateCommand(ChangeColorExecute); }
+        }
+
+        private void ChangeColorExecute()
+        {
+            AppBackground = new SolidColorBrush(Colors.Pink);
+        }
+
         public override Task OnNavigatedTo(NavigationEventArgs args)
         {
             HolaMundo = "Hola mundo desde el ViewModel!!!";
+            AppBackground = new SolidColorBrush(Colors.Azure);
             return null;
         }
 
